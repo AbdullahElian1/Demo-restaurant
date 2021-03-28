0d834b6109333cfc6d6b8f3691a821891fdd2a6e
AbdullahElian1 <abodelian28@gmail.com>
2021-03-28 14:14:50 +0300

    add loop

--- javaScript/app.js
+++ javaScript/app.js
@@ -4,14 +4,32 @@ var firstName = prompt("enter your name ");
 alert("Welcome to our restaurant " + firstName);
 var favFood = prompt("enter your favorite food","burger, shawerma");
 
-if(favFood === "burger"){
-    document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg" />');
+while (favFood !== "burger" && favFood !== "shawerma")
+{
+    favFood= prompt("please select right food ", "burger, shawerma");
 }
 
-else if (favFood === "shawerma"){
- document.write('<img src="https://i1.wp.com/2qlam.com/wp-content/uploads/2020/04/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9-%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%B4%D8%A7%D9%88%D8%B1%D9%85%D8%A7.jpg?fit=780%2C521&ssl=1 />"');
-}
-else{
-    alert(" Please select the right food ");
+var foodNum= prompt("how many burger or shawerma woluld like to see");
+var image ='';
+console.log(image);
+for(var i=1; i<=foodNum;i++)
+{
+    if(favFood === "burger"){
+        
+        image += '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg" />';
+    }
+    
+    else if (favFood === "shawerma"){
+   
+     image += '<img src="https://i1.wp.com/2qlam.com/wp-content/uploads/2020/04/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9-%D8%B9%D9%85%D9%84-%D8%A7%D9%84%D8%B4%D8%A7%D9%88%D8%B1%D9%85%D8%A7.jpg?fit=780%2C521&ssl=1" />';
+    }
+    else{
+        alert(" Please select the right food ");
+    
+    }
+    
 
 }
+console.log(image);
+document.write(image);
+
